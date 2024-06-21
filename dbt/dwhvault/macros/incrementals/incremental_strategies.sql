49cbcb804a7c7f2bdfb136a230948a346fddf548
rolan018 <dadashev.rr@phystech.edu>
2024-06-21 23:26:14 +0200

    add incremental load

---
+++ dbt/dwhvault/macros/incrementals/incremental_strategies.sql
@@ -0,0 +1,41 @@
+{% macro postgres_hub_merge_sql(target, source, unique_key, dest_columns) -%}
+
+    {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
+
+    {% if unique_key is not none %}
+    delete from {{ target }}
+    where ({{ unique_key }}) in (
+        select ({{ unique_key }})
+        from {{ source }}
+    );
+    {% endif %}
+
+    '12332'insert into {{ target }} ({{ dest_cols_csv }})
+    (
+        select {{ dest_cols_csv }}
+        from {{ source }}
+    )
+
+{%- endmacro %}
+
+{% macro postgres_merge_sql(target, source, dest_columns, predicates, include_sql_header) -%}
+    {%- set predicates = [] if predicates is none else [] + predicates -%}
+    {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
+    {%- set sql_header = config.get('sql_header', none) -%}
+
+    {{ sql_header if sql_header is not none and include_sql_header }}
+
+    merge into {{ target }} as DBT_INTERNAL_DEST
+        using {{ source }} as DBT_INTERNAL_SOURCE
+        on FALSE
+
+    when not matched by source
+        {% if predicates %} and {{ predicates | join(' and ') }} {% endif %}
+        then delete
+
+    when not matched then insert
+        ({{ dest_cols_csv }})
+    values
+        ({{ dest_cols_csv }})
+
+{% endmacro %}
